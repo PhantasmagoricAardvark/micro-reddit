213f64c90184014ae87cd4be7319e48195456b8f
seba <villeaho@tutanota.com>
2019-03-06 23:41:39 +0200

    A comments  in a post is now attributed to the correct user. Each comment belongs to a post. A post has comments. In short, the associations are now correct.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
-belongs_to :post
+	belongs_to :post
+	belongs_to :user
+	validates :content, presence: true, length: { minimum: 2 }
+	validates :user_id, :post_id, presence: true, numericality: { only_integer: true } 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,14 +9,16 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-# ADD FOREIGN KEY TO COMMENTS
 
-ActiveRecord::Schema.define(version: 2019_03_02_211059) do
+ActiveRecord::Schema.define(version: 2019_03_03_191851) do
 
   create_table "comments", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
